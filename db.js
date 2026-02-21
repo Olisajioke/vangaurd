6c432b526b983fa2ab91e5bf7a0d72c64b82c874
Olisajioke <chijioke914@gmail.com>
2026-02-21 14:25:09 +0200

    CHANGING DATABASE HOST

--- db.js
+++ db.js
@@ -2,14 +2,19 @@ import dotenv from "dotenv";
 import mysql from "mysql2/promise"
 
 dotenv.config();
+const db = mysql.createPool(process.env.DATABASE_URL);
 
+export default db;
+/** 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,   // MySQL password
-  database: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   connectionLimit: 10
 })
 
 export default db
+
+*/
